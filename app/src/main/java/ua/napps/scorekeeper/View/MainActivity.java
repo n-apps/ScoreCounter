7fde69742a572e40b170919fcd7ce3bc9d2cef0f
roman novodvorskiy <roman.novodvorskiy@gmail.com>
2015-12-29 08:30:23 +0000

    MainActivity.java edited online with Bitbucket

--- app/src/main/java/ua/napps/scorekeeper/View/MainActivity.java
+++ app/src/main/java/ua/napps/scorekeeper/View/MainActivity.java
@@ -215,6 +215,17 @@ public class MainActivity extends AppCompatActivity implements MainView {
         mCountersRecyclerView.invalidate();
     }
 
+/*
+TODO: // This method hides the system bars and resize the content
+  private void hideSystemUI() {
+    getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
+            | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
+            // remove the following flag for version < API 19
+            | View.SYSTEM_UI_FLAG_IMMERSIVE); 
+  } 
+*/
+
     @Override
     public void toggleKeepScreenOn(boolean isSelected) {
         if (isSelected) getWindow().addFlags(FLAG_KEEP_SCREEN_ON);
