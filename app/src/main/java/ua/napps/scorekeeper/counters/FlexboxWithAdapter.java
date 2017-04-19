1ed2145cca411f9052fe6930d5845fbbac4faa75
roman <roman.novodvorskiy@gmail.com>
2017-04-19 23:08:40 +0300

    Update FlexboxWithAdapter

--- app/src/main/java/ua/napps/scorekeeper/counters/FlexboxWithAdapter.java
+++ app/src/main/java/ua/napps/scorekeeper/counters/FlexboxWithAdapter.java
@@ -1,7 +1,6 @@
 package ua.napps.scorekeeper.counters;
 
 import android.content.Context;
-import android.databinding.BindingAdapter;
 import android.databinding.DataBindingUtil;
 import android.databinding.ObservableList;
 import android.databinding.ViewDataBinding;
@@ -14,23 +13,20 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import com.google.android.flexbox.FlexboxLayout;
+import com.google.android.flexbox.FlexboxLayoutManager;
 import java.util.List;
-import ua.com.napps.scorekeeper.BR;
 import ua.com.napps.scorekeeper.R;
+import ua.com.napps.scorekeeper.databinding.ItemCounterBinding;
 
 public class FlexboxWithAdapter extends FlexboxLayout {
 
     private static final String TAG = "FlexboxWithAdapter";
 
     private CounterActionCallback callback;
+    private List<Counter> counters;
 
     public FlexboxWithAdapter(Context context) {
         super(context);
-        init();
-    }
-
-    private void init() {
-
     }
 
     public FlexboxWithAdapter(Context context, AttributeSet attrs) {
@@ -41,51 +37,28 @@ public class FlexboxWithAdapter extends FlexboxLayout {
         super(context, attrs, defStyleAttr);
     }
 
-    /**
-     * Assign a list of items to a ViewGroup. This is used with the {@code entries} and
-     * {@code layout} attributes in the application namespace. Example Usage:
-     * <pre><code>&lt;LinearLayout
-     *     android:layout_width="match_parent"
-     *     android:layout_height="wrap_content"
-     *     android:orientation="vertical"
-     *     app:entries="@{items}"
-     *     app:layout="@{@layout/item}"/&gt;
-     * </code></pre>
-     * <p>
-     * In the above, {@code items} is a List or ObservableList. {@code layout} does not
-     * need to be hard-coded, but most commonly will be. This BindingAdapter will work
-     * with any ViewGroup that only needs addView() and removeView() to manage its Views.
-     * <p>
-     * The layout, &commat;layout/item for example, must have a single variable named
-     * {@code data}.
-     */
-
-    @BindingAdapter({ "entries", "layout" }) public static <T> void setEntries(ViewGroup viewGroup,
-            List<T> oldEntries, int oldLayoutId, List<T> newEntries, int newLayoutId) {
-        if (oldEntries == newEntries && oldLayoutId == newLayoutId) {
-            return; // nothing has changed
+    public void setEntries(List<Counter> newEntries) {
+        if (counters == newEntries) {
+            return;
         }
+        counters = newEntries;
 
-        EntryChangeListener listener = ListenerUtil.getListener(viewGroup, R.id.entryListener);
-        if (oldEntries != newEntries && listener != null && oldEntries instanceof ObservableList) {
-            ((ObservableList) oldEntries).removeOnListChangedCallback(listener);
+        EntryChangeListener listener = ListenerUtil.getListener(this, R.id.entryListener);
+        if (listener != null && counters instanceof ObservableList) {
+            ((ObservableList) counters).removeOnListChangedCallback(listener);
         }
 
         if (newEntries == null) {
-            viewGroup.removeAllViews();
+            removeAllViews();
         } else {
             if (newEntries instanceof ObservableList) {
                 if (listener == null) {
-                    listener = new EntryChangeListener(viewGroup, newLayoutId);
-                    ListenerUtil.trackListener(viewGroup, listener, R.id.entryListener);
-                } else {
-                    listener.setLayoutId(newLayoutId);
+                    listener = new EntryChangeListener(this);
+                    ListenerUtil.trackListener(this, listener, R.id.entryListener);
                 }
-                if (newEntries != oldEntries) {
-                    ((ObservableList) newEntries).addOnListChangedCallback(listener);
-                }
+                ((ObservableList) newEntries).addOnListChangedCallback(listener);
             }
-            resetViews(viewGroup, newLayoutId, newEntries);
+            resetViews();
         }
     }
 
@@ -93,97 +66,68 @@ public class FlexboxWithAdapter extends FlexboxLayout {
         this.callback = callback;
     }
 
-    /**
-     * Starts a transition only if on KITKAT or higher.
-     *
-     * @param root The scene root
-     */
-    private static void startTransition(ViewGroup root) {
+    private void startTransition(ViewGroup root) {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
             TransitionManager.beginDelayedTransition(root);
         }
     }
 
-    /**
-     * Clears all Views in {@code parent} and fills it with a View for
-     * each item in {@code entries}, bound to the item. If layoutId
-     * is 0, no Views will be added.
-     *
-     * @param parent The ViewGroup to contain the list of items.
-     * @param layoutId The layout ID to inflate for the child Views.
-     * @param entries The list of items to bind to the inflated Views. Each
-     * item will be bound to a different child View.
-     */
-    private static void resetViews(ViewGroup parent, int layoutId, List entries) {
-        parent.removeAllViews();
-        if (layoutId == 0) {
-            return;
-        }
-        LayoutInflater inflater = (LayoutInflater) parent.getContext()
-                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        for (int i = 0; i < entries.size(); i++) {
-            Object entry = entries.get(i);
-            ViewDataBinding binding = bindLayout(inflater, parent, layoutId, entry);
-            //binding.setCallback(callback);
-            parent.addView(binding.getRoot());
+    private void resetViews() {
+        removeAllViews();
+
+        LayoutInflater inflater =
+                (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        for (int i = 0; i < counters.size(); i++) {
+            ItemCounterBinding binding = bindLayout(inflater, counters.get(i), callback);
+            final View root = binding.getRoot();
+            ViewGroup.LayoutParams lp = root.getLayoutParams();
+            if (lp instanceof FlexboxLayoutManager.LayoutParams) {
+                FlexboxLayoutManager.LayoutParams flexboxLp =
+                        (FlexboxLayoutManager.LayoutParams) lp;
+                flexboxLp.setFlexGrow(1.0f);
+                flexboxLp.setFlexShrink(1.0f);
+                flexboxLp.setFlexBasisPercent(-1);
+            }
+            addView(root);
         }
     }
 
-    /**
-     * Inflates and binds a layout to an entry to the {@code data} variable
-     * of the bound layout.
-     *
-     * @param inflater The LayoutInflater
-     * @param parent The ViewGroup containing the list of Views
-     * @param layoutId The layout ID to use for the list item
-     * @param entry The data to bind to the inflated View
-     * @return A ViewDataBinding, bound to a newly-inflated View with {@code entry}
-     * set as the {@code data} variable.
-     */
-    private static ViewDataBinding bindLayout(LayoutInflater inflater, ViewGroup parent,
-            int layoutId, Object entry) {
-        ViewDataBinding binding = DataBindingUtil.inflate(inflater, layoutId, parent, false);
-        if (!binding.setVariable(BR.data, entry)) {
-            String layoutName = parent.getResources().getResourceEntryName(layoutId);
+    private ItemCounterBinding bindLayout(LayoutInflater inflater, Object counter,
+            CounterActionCallback callback) {
+        ItemCounterBinding binding =
+                DataBindingUtil.inflate(inflater, R.layout.item_counter, this, false);
+        if (!binding.setVariable(ua.com.napps.scorekeeper.BR.data, counter)) {
+            String layoutName = getResources().getResourceEntryName(R.layout.item_counter);
             Log.w(TAG, "There is no variable 'data' in layout " + layoutName);
+        }
+        if (!binding.setVariable(ua.com.napps.scorekeeper.BR.callback, callback)) {
+            String layoutName = getResources().getResourceEntryName(R.layout.item_counter);
+            Log.w(TAG, "There is no variable 'callback' in layout " + layoutName);
         }
         return binding;
     }
 
-    /**
-     * A listener to watch for changes in an Observable list and
-     * animate the change.
-     */
-    private static class EntryChangeListener extends ObservableList.OnListChangedCallback {
+    private class EntryChangeListener extends ObservableList.OnListChangedCallback {
         private final ViewGroup mTarget;
-        private int mLayoutId;
 
-        public EntryChangeListener(ViewGroup target, int layoutId) {
+        EntryChangeListener(ViewGroup target) {
             mTarget = target;
-            mLayoutId = layoutId;
-        }
-
-        public void setLayoutId(int layoutId) {
-            mLayoutId = layoutId;
         }
 
         @Override public void onChanged(ObservableList observableList) {
-            resetViews(mTarget, mLayoutId, observableList);
+            resetViews();
         }
 
         @Override
         public void onItemRangeChanged(ObservableList observableList, int start, int count) {
-            if (mLayoutId == 0) {
-                return;
-            }
             LayoutInflater inflater = (LayoutInflater) mTarget.getContext()
                     .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
             startTransition(mTarget);
             final int end = start + count;
             for (int i = start; i < end; i++) {
                 Object data = observableList.get(i);
-                ViewDataBinding binding = bindLayout(inflater, mTarget, mLayoutId, data);
-                binding.setVariable(BR.data, observableList.get(i));
+                ViewDataBinding binding = bindLayout(inflater, data, callback);
+                binding.setVariable(ua.com.napps.scorekeeper.BR.data, observableList.get(i));
                 mTarget.removeViewAt(i);
                 mTarget.addView(binding.getRoot(), i);
             }
@@ -191,25 +135,28 @@ public class FlexboxWithAdapter extends FlexboxLayout {
 
         @Override
         public void onItemRangeInserted(ObservableList observableList, int start, int count) {
-            if (mLayoutId == 0) {
-                return;
-            }
             startTransition(mTarget);
             final int end = start + count;
             LayoutInflater inflater = (LayoutInflater) mTarget.getContext()
                     .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
             for (int i = end - 1; i >= start; i--) {
                 Object entry = observableList.get(i);
-                ViewDataBinding binding = bindLayout(inflater, mTarget, mLayoutId, entry);
-                mTarget.addView(binding.getRoot(), start);
+                ViewDataBinding binding = bindLayout(inflater, entry, callback);
+                final View root = binding.getRoot();
+                //ViewGroup.LayoutParams lp = root.getLayoutParams();
+                //if (lp instanceof FlexboxLayout.LayoutParams) {
+                //    FlexboxLayout.LayoutParams flexboxLp = (FlexboxLayout.LayoutParams) lp;
+                //    flexboxLp.setFlexGrow(1.0f);
+                //    flexboxLp.setFlexShrink(1.0f);
+                //    flexboxLp.setFlexBasisPercent(-1);
+                //    root.setLayoutParams(flexboxLp);
+                //}
+                mTarget.addView(root, start);
             }
         }
 
         @Override
         public void onItemRangeMoved(ObservableList observableList, int from, int to, int count) {
-            if (mLayoutId == 0) {
-                return;
-            }
             startTransition(mTarget);
             for (int i = 0; i < count; i++) {
                 View view = mTarget.getChildAt(from);
@@ -221,9 +168,6 @@ public class FlexboxWithAdapter extends FlexboxLayout {
 
         @Override
         public void onItemRangeRemoved(ObservableList observableList, int start, int count) {
-            if (mLayoutId == 0) {
-                return;
-            }
             startTransition(mTarget);
             for (int i = 0; i < count; i++) {
                 mTarget.removeViewAt(start);
