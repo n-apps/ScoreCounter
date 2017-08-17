620b8be1bf970599a34bacc4feb919f36d25da5a
roman <roman.novodvorskiy@gmail.com>
2017-08-18 00:12:54 +0300

    Use recyclerview; Add timber; Lock orientation

--- app/src/main/java/ua/napps/scorekeeper/counters/CounterActionCallback.java
+++ app/src/main/java/ua/napps/scorekeeper/counters/CounterActionCallback.java
@@ -13,5 +13,5 @@ public interface CounterActionCallback {
 
   void onDecreaseClick(String id);
 
-  void onCounterAdded(View v);
+  void onCounterAdded();
 }
--- app/src/main/java/ua/napps/scorekeeper/utils/DataBindingAdapters.java
+++ app/src/main/java/ua/napps/scorekeeper/utils/DataBindingAdapters.java
@@ -9,8 +9,13 @@ import android.support.v4.graphics.drawable.DrawableCompat;
 import android.support.v7.content.res.AppCompatResources;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import com.google.android.flexbox.FlexDirection;
+import com.google.android.flexbox.FlexWrap;
+import com.google.android.flexbox.FlexboxLayoutManager;
 import ua.napps.scorekeeper.counters.Counter;
+import ua.napps.scorekeeper.counters.CounterActionCallback;
 import ua.napps.scorekeeper.counters.CountersAdapter;
+import ua.napps.scorekeeper.data.CurrentSet;
 
 public class DataBindingAdapters {
 
@@ -33,11 +38,42 @@ public class DataBindingAdapters {
     view.setOnTouchListener(new RepeatListener(onClickListener, null));
   }
 
-  @BindingAdapter("bind:items")
-  public static void items(RecyclerView rv, ObservableArrayList<Counter> observableArrayList) {
-    CountersAdapter rvAdapter = (CountersAdapter) rv.getAdapter();
-    if (rvAdapter != null) {
-      rvAdapter.changeItems(observableArrayList, rv.getHeight());
+  @BindingAdapter({ "bind:items", "bind:callback" })
+  public static void items(RecyclerView recyclerView, ObservableArrayList<Counter> newEntries,
+      CounterActionCallback callback) {
+    CountersAdapter rvAdapter = (CountersAdapter) recyclerView.getAdapter();
+
+    if (rvAdapter == null) {
+      CountersAdapter adapter =
+          new CountersAdapter(callback, CurrentSet.getInstance().getCounters());
+      if (CurrentSet.getInstance().getSize() > 5) {
+        FlexboxLayoutManager layoutManager =
+            new FlexboxLayoutManager(recyclerView.getContext(), FlexDirection.COLUMN,
+                FlexWrap.NOWRAP);
+        recyclerView.setLayoutManager(layoutManager);
+      } else {
+        FlexboxLayoutManager layoutManager = new FlexboxLayoutManager(recyclerView.getContext());
+        recyclerView.setLayoutManager(layoutManager);
+      }
+      recyclerView.setAdapter(adapter);
+    }
+
+    final int size = newEntries.size();
+    if (size <= 4) {
+      if (((FlexboxLayoutManager) recyclerView.getLayoutManager()).getFlexWrap()
+          != FlexWrap.NOWRAP) {
+        FlexboxLayoutManager layoutManager =
+            new FlexboxLayoutManager(recyclerView.getContext(), FlexDirection.COLUMN,
+                FlexWrap.NOWRAP);
+        recyclerView.setLayoutManager(layoutManager);
+      }
+    } else {
+      if (((FlexboxLayoutManager) recyclerView.getLayoutManager()).getFlexWrap() != FlexWrap.WRAP) {
+        FlexboxLayoutManager layoutManager =
+            new FlexboxLayoutManager(recyclerView.getContext(), FlexDirection.ROW, FlexWrap.WRAP);
+        recyclerView.setLayoutManager(layoutManager);
+      }
+      recyclerView.smoothScrollToPosition(size - 1);
     }
   }
 }
