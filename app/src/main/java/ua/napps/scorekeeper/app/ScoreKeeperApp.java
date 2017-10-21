8a1a568b645b52c7f8cbeb2aeafbec63c7bdb529
roman <roman.novodvorskiy@gmail.com>
2017-10-21 23:34:20 +0300

    Prepare migration

--- app/src/main/java/ua/napps/scorekeeper/app/ScoreKeeperApp.java
+++ app/src/main/java/ua/napps/scorekeeper/app/ScoreKeeperApp.java
@@ -1,6 +1,7 @@
 package ua.napps.scorekeeper.app;
 
 import android.app.Application;
+import android.content.pm.PackageManager.NameNotFoundException;
 import android.support.v7.app.AppCompatDelegate;
 import timber.log.Timber;
 import ua.com.napps.scorekeeper.BuildConfig;
@@ -8,12 +9,29 @@ import ua.napps.scorekeeper.storage.DatabaseHolder;
 
 public class ScoreKeeperApp extends Application {
 
-  @Override public void onCreate() {
-    super.onCreate();
-    AppCompatDelegate.setCompatVectorFromResourcesEnabled(true);
-    if (BuildConfig.DEBUG) {
-      Timber.plant(new Timber.DebugTree());
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        AppCompatDelegate.setCompatVectorFromResourcesEnabled(true);
+        if (BuildConfig.DEBUG) {
+            Timber.plant(new Timber.DebugTree());
+        }
+        DatabaseHolder.init(this);
+
+        if (getVersionCode() < 30000) {
+
+        }
+
     }
-    DatabaseHolder.init(this);
-  }
+
+    private int getVersionCode() {
+        int result = 0;
+        try {
+            result = getApplicationContext().getPackageManager().getPackageInfo(
+                    getApplicationContext().getPackageName(), 0).versionCode;
+        } catch (NameNotFoundException e) {
+            // squelch
+        }
+        return result;
+    }
 }
