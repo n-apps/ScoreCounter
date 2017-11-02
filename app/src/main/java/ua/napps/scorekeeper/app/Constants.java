ba1083cbdfbc01189317247dc9aafabfa2f7090a
roman <roman.novodvorskiy@gmail.com>
2017-11-02 23:04:59 +0200

    Add spring animation

--- app/src/main/java/ua/napps/scorekeeper/app/Constants.java
+++ app/src/main/java/ua/napps/scorekeeper/app/Constants.java
@@ -1,11 +1,10 @@
 package ua.napps.scorekeeper.app;
 
 public final class Constants {
-  public static final String SETTINGS = "settings";
 
     public static final String SETTINGS_STAY_AWAKE = "stay_awake";
 
-  public static final int MAX_COUNTERS_TO_FIT_ON_SCREEN = 5;
+    public static final int MAX_COUNTERS_TO_FIT_ON_SCREEN = 5;
 
-  public static final int DEFAULT_COUNTER_HEIGHT = 384;
+    public static final int DEFAULT_COUNTER_HEIGHT = 384;
 }
--- app/src/main/java/ua/napps/scorekeeper/utils/ColorUtil.java
+++ app/src/main/java/ua/napps/scorekeeper/utils/ColorUtil.java
@@ -1,46 +1,69 @@
 package ua.napps.scorekeeper.utils;
 
 import android.graphics.Color;
+import android.support.annotation.ColorInt;
+import android.support.annotation.FloatRange;
 import java.util.Random;
 
 public final class ColorUtil {
 
-  public ColorUtil() {
-  }
-
-  public static boolean isDarkBackground(String background) {
-    final int argbColor = Color.parseColor(background);
-    int r = Color.red(argbColor);
-    int g = Color.green(argbColor);
-    int b = Color.blue(argbColor);
-    int o = ((r * 299) + (g * 587) + (b * 114)) / 1000;
-    return o < 125;
-  }
-
-  public static String getRandomColor() {
-    double goldenRatioConj = (1.0 + Math.sqrt(5.0)) / 2.0;
-    float saturation;
-    float hue;
-    long seed = new Random().nextLong();
-    saturation = randFloat(0.5f, 0.9f, seed);
-    hue = new Random(seed).nextFloat();
-    hue += goldenRatioConj;
-    hue = hue % 1;
-
-    final int color = Color.HSVToColor(new float[] { hue * 360, saturation, 0.9f });
-    return intColorToString(color);
-  }
-
-  public static String intColorToString(int color) {
-    return String.format("#%06X", 0xFFFFFF & color);
-  }
-
-  private static float randFloat(float min, float max, long seed) {
-    Random generator = new Random(seed);
-    if (generator.nextDouble() < 0.5) {
-      return (float) (((1 - generator.nextDouble()) * (max - min)) + min);
+    public static float constrain(float min, float max, float v) {
+        return Math.max(min, Math.min(max, v));
     }
 
-    return (float) ((generator.nextDouble() * (max - min)) + min);
-  }
+    public static String getRandomColor() {
+        double goldenRatioConj = (1.0 + Math.sqrt(5.0)) / 2.0;
+        float saturation;
+        float hue;
+        long seed = new Random().nextLong();
+        saturation = randFloat(0.5f, 0.9f, seed);
+        hue = new Random(seed).nextFloat();
+        hue += goldenRatioConj;
+        hue = hue % 1;
+
+        final int color = Color.HSVToColor(new float[]{hue * 360, saturation, 0.9f});
+        return intColorToString(color);
+    }
+
+    public static String intColorToString(int color) {
+        return String.format("#%06X", 0xFFFFFF & color);
+    }
+
+    public static boolean isDarkBackground(String background) {
+        final int argbColor = Color.parseColor(background);
+        int r = Color.red(argbColor);
+        int g = Color.green(argbColor);
+        int b = Color.blue(argbColor);
+        int o = ((r * 299) + (g * 587) + (b * 114)) / 1000;
+        return o < 125;
+    }
+
+    public static @ColorInt
+    int scrimify(@ColorInt int color,
+            boolean isDark,
+            @FloatRange(from = 0f, to = 1f) float lightnessMultiplier) {
+        float[] hsl = new float[3];
+        android.support.v4.graphics.ColorUtils.colorToHSL(color, hsl);
+
+        if (!isDark) {
+            lightnessMultiplier += 1f;
+        } else {
+            lightnessMultiplier = 1f - lightnessMultiplier;
+        }
+
+        hsl[2] = constrain(0f, 1f, hsl[2] * lightnessMultiplier);
+        return android.support.v4.graphics.ColorUtils.HSLToColor(hsl);
+    }
+
+    public ColorUtil() {
+    }
+
+    private static float randFloat(float min, float max, long seed) {
+        Random generator = new Random(seed);
+        if (generator.nextDouble() < 0.5) {
+            return (float) (((1 - generator.nextDouble()) * (max - min)) + min);
+        }
+
+        return (float) ((generator.nextDouble() * (max - min)) + min);
+    }
 }
