58f9a9f5c3bb3cd31c8c751dfc5ecc93ce877b1a
Roman Shuliatiev <roman.novodvorskiy@gmail.com>
2018-02-17 11:53:38 +0200

    Move counters activity logic inside fragment; Update room dependency

--- app/src/main/java/ua/napps/scorekeeper/dice/DicesFragment.java
+++ app/src/main/java/ua/napps/scorekeeper/dice/DicesFragment.java
@@ -20,7 +20,6 @@ public class DicesFragment extends Fragment {
     }
 
     public static DicesFragment newInstance() {
-
         return new DicesFragment();
     }
 
@@ -52,16 +51,6 @@ public class DicesFragment extends Fragment {
         mListener = null;
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
     public interface OnDiceFragmentInteractionListener {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
