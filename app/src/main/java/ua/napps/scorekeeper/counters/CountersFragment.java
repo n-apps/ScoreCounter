36470233fd76941d7c70cfe1ad8431b50fd1b837
Roman Shuliatiev <roman.novodvorskiy@gmail.com>
2018-03-02 00:28:00 +0200

    Improvements to night theme

--- app/src/main/java/ua/napps/scorekeeper/counters/CountersFragment.java
+++ app/src/main/java/ua/napps/scorekeeper/counters/CountersFragment.java
@@ -303,6 +303,7 @@ public class CountersFragment extends Fragment implements CounterActionCallback 
                 .content(R.string.counter_details_name)
                 .inputType(InputType.TYPE_TEXT_FLAG_CAP_CHARACTERS | InputType.TYPE_TEXT_FLAG_AUTO_COMPLETE | InputType.TYPE_TEXT_FLAG_AUTO_CORRECT | InputType.TYPE_TEXT_VARIATION_PERSON_NAME)
                 .positiveText(R.string.common_set)
+                .negativeColorRes(R.color.primaryColor)
                 .negativeText(R.string.common_cancel)
                 .input(counter.getName(), null, true,
                         (dialog, input) -> {
