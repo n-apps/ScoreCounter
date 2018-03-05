206e1ab151c4537c91f978ec1e23cbd4446c2281
Roman Shuliatiev <roman.novodvorskiy@gmail.com>
2018-03-06 00:23:58 +0200

    Add reveal transition (APi 21+)

---
+++ app/src/main/java/ua/napps/scorekeeper/utils/RoundedColorView.java
@@ -0,0 +1,6 @@
+package ua.napps.scorekeeper.utils;
+
+import android.view.View;
+
+public class RoundedColorView extends View {
+}
---
+++ app/src/main/java/ua/napps/scorekeeper/utils/TransitionListenerAdapter.java
@@ -0,0 +1,6 @@
+package ua.napps.scorekeeper.utils;
+
+import android.transition.Transition;
+
+public abstract class TransitionListenerAdapter implements Transition.TransitionListener {
+}
