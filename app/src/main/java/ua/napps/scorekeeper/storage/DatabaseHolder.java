8f744c503be6d3167af0cea24ac3e88c9ab3995f
Roman Shuliatiev <roman.novodvorskiy@gmail.com>
2018-03-15 23:13:16 +0200

    Add migration

--- app/src/main/java/ua/napps/scorekeeper/storage/DatabaseHolder.java
+++ app/src/main/java/ua/napps/scorekeeper/storage/DatabaseHolder.java
@@ -10,7 +10,7 @@ import android.support.annotation.NonNull;
 import ua.napps.scorekeeper.counters.Counter;
 import ua.napps.scorekeeper.counters.CountersDao;
 
-@Database(entities = {Counter.class}, version = 1, exportSchema = false)
+@Database(entities = {Counter.class}, version = 2, exportSchema = false)
 public abstract class DatabaseHolder extends RoomDatabase {
 
     private static DatabaseHolder database;
@@ -23,9 +23,17 @@ public abstract class DatabaseHolder extends RoomDatabase {
     @MainThread
     public static void init(@NonNull Context context) {
         database = Room.databaseBuilder(context.getApplicationContext(), DatabaseHolder.class, "counters-database")
-                .fallbackToDestructiveMigration()
+                .addMigrations(MIGRATION_1_2)
                 .build();
     }
 
     public abstract CountersDao countersDao();
+
+
+    static final android.arch.persistence.room.migration.Migration MIGRATION_1_2 = new android.arch.persistence.room.migration.Migration(1, 2) {
+    @Override
+    public void migrate(android.arch.persistence.db.SupportSQLiteDatabase database) {
+        // Since we didn't alter the table, there's nothing else to do here.
+    }
+};
 }
