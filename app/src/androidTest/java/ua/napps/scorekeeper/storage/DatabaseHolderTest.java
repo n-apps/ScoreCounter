80c8f96ba12f12fe5491c6a7b07659adb2851eaf
Roman Shuliatiev <roman.novodvorskiy@gmail.com>
2018-03-20 23:58:07 +0200

    Add test libs

--- app/src/androidTest/java/ua/napps/scorekeeper/storage/DatabaseHolderTest.java
+++ app/src/androidTest/java/ua/napps/scorekeeper/storage/DatabaseHolderTest.java
@@ -1,16 +1,20 @@
 package ua.napps.scorekeeper.storage;
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
 import android.arch.persistence.room.Room;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
-import ua.napps.scorekeeper.counters.Counter;
-import java.util.List;
-import static org.junit.Assert.assertThat;
-import static org.junit.Assert.assertEquals;
+
 import org.hamcrest.core.Is;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import java.util.List;
+
+import ua.napps.scorekeeper.counters.Counter;
+
+import static junit.framework.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
 
 @RunWith(AndroidJUnit4.class)
 public class DatabaseHolderTest {
