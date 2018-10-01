54ba8a2d632728c480768c41bd2fe8c6b45418a6
vabene1111 <vabene1234@googlemail.com>
2018-10-01 15:24:30 +0200

    basic log types and view

--- app/src/main/java/ua/napps/scorekeeper/log/LogAdapter.java
+++ app/src/main/java/ua/napps/scorekeeper/log/LogAdapter.java
@@ -8,9 +8,10 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import java.text.DecimalFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Locale;
 
 import ua.com.napps.scorekeeper.R;
 import ua.napps.scorekeeper.utils.RoundedColorView;
@@ -19,12 +20,13 @@ public class LogAdapter extends RecyclerView.Adapter<LogAdapter.LogAdapterViewHo
     private ArrayList<LogEntry> logEntries;
 
     static class LogAdapterViewHolder extends RecyclerView.ViewHolder {
-        TextView tv_main,tv_time;
+        TextView tv_counter,tv_info,tv_time;
         RoundedColorView rcv_color;
 
         LogAdapterViewHolder(View v) {
             super(v);
-            tv_main = v.findViewById(R.id.tv_item_log_main);
+            tv_counter = v.findViewById(R.id.tv_item_log_counter);
+            tv_info = v.findViewById(R.id.tv_item_log_info);
             tv_time = v.findViewById(R.id.tv_item_log_time);
             rcv_color = v.findViewById(R.id.rcv_item_log_color);
         }
@@ -46,8 +48,38 @@ public class LogAdapter extends RecyclerView.Adapter<LogAdapter.LogAdapterViewHo
     public void onBindViewHolder(@NonNull LogAdapterViewHolder holder, int position) {
         LogEntry logEntry = logEntries.get(position);
 
-        holder.tv_main.setText(logEntry.toString());
+        holder.tv_counter.setText(logEntry.counter.getName());
 
+        String info = "";
+        switch (logEntry.type){
+            case INC:
+                info = "\u2795 ";
+                break;
+            case INC_C:
+                info = "\u2795 ";
+                break;
+            case DEC:
+                info = "\u2796 ";
+                break;
+            case DEC_C:
+                info = "\u2796 ";
+                break;
+            case SET:
+                info = "\uD83D\uDD8A ";
+                break;
+            case RMV:
+                info = "\uD83D\uDDD1 ";
+                break;
+            case RST:
+                info = "\uD83D\uDD04 ";
+                break;
+        }
+
+        DecimalFormat decimalFormat = (DecimalFormat)DecimalFormat.getNumberInstance(Locale.ENGLISH); // TODO use user locale
+
+        info = info + decimalFormat.format(logEntry.value) + " [" + decimalFormat.format(logEntry.prevValue) + " \u27A1 " + decimalFormat.format(logEntry.prevValue + logEntry.value) + "]";
+
+        holder.tv_info.setText(info);
 
         SimpleDateFormat format = new SimpleDateFormat("HH:mm:ss");
         String formatTime = format.format(logEntry.timestamp);
