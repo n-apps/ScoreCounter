978cf218c2162f0f13510403d5697585cae583a6
vabene1111 <vabene1234@googlemail.com>
2020-09-25 13:29:00 +0200

    added dice composition
    show the composition of the dice value after roll when rolling with more than one dice

--- app/src/main/java/ua/napps/scorekeeper/dice/DiceLiveData.java
+++ app/src/main/java/ua/napps/scorekeeper/dice/DiceLiveData.java
@@ -3,10 +3,13 @@ package ua.napps.scorekeeper.dice;
 import androidx.annotation.IntRange;
 import androidx.lifecycle.LiveData;
 
+import java.util.ArrayList;
+
 class DiceLiveData extends LiveData<Integer> {
 
     private int diceSides;
     private int diceCount;
+    private ArrayList<Integer> rolls;
 
     DiceLiveData() {
         setValue(0);
@@ -21,12 +24,19 @@ class DiceLiveData extends LiveData<Integer> {
     }
 
     void rollDice() {
+        this.rolls = new ArrayList<>();
         int total = 0;
+
         for (int i = 0; i < diceCount; i++) {
             int roll = ((int) (Math.random() * diceSides)) + 1;
             total += roll;
+            this.rolls.add(roll);
         }
         setValue(total);
+    }
+
+    public ArrayList<Integer> getRolls(){
+        return this.rolls;
     }
 
     public void setDiceSides(@IntRange(from = 1, to = 100) int diceSides) {
