8ee4076caa499e9ac41c7923dc0dbd6a82f0c664
Naik <naiksav4enko@gmail.com>
2022-06-05 09:06:53 +0300

    Improve workflow script

--- tools/generate-test-env.sh
+++ tools/generate-test-env.sh
@@ -8,8 +8,7 @@ echo "import 'package:firebase_core/firebase_core.dart' show FirebaseOptions;
 class DefaultFirebaseOptions {
   static FirebaseOptions get currentPlatform {
     throw UnsupportedError(
-      'DefaultFirebaseOptions have not been configured for macos - '
-          'you can reconfigure this by running the FlutterFire CLI again.',
+      'DefaultFirebaseOptions have not been configured for tests',
     );
   }
 }" > lib/firebase_options.dart
