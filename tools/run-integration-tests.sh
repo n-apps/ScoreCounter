ca1d8553a9a84a243daa8bf9e5ef5fcd165f3803
Naik <naiksav4enko@gmail.com>
2022-06-06 08:04:00 +0300

    Improve integration test results

--- tools/run-integration-tests.sh
+++ tools/run-integration-tests.sh
@@ -3,6 +3,4 @@
 # Copyright (c) 2022 Score Counter
 #
 
-flutter drive \
-  --driver=test_driver/integration_test.dart \
-  --target=integration_test/app_test.dart
+flutter test integration_test/app_test.dart
