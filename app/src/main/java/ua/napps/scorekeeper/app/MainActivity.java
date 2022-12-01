2f2ea82a94abaeb549c88d77bab75dbd47d24cd7
Roman S <roman.novodvorskiy@gmail.com>
2022-12-01 22:05:47 +0200

    v 7.1.7
    - More snow

--- app/src/main/java/ua/napps/scorekeeper/app/MainActivity.java
+++ app/src/main/java/ua/napps/scorekeeper/app/MainActivity.java
@@ -86,17 +86,14 @@ public class MainActivity extends AppCompatActivity implements SharedPreferences
                     } else {
                         hideDiceBadge();
                     }
-                    bottomNavigationBar.setElevation(0);
                     break;
                 case R.id.dices:
                     switchFragment(TAGS[1]);
                     hideDiceBadge();
-                    bottomNavigationBar.setElevation(0);
                     break;
                 case R.id.more:
                     switchFragment(TAGS[2]);
                     hideDiceBadge();
-                    bottomNavigationBar.setElevation(20);
                     break;
             }
 
