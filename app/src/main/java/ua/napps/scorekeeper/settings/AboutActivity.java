51de6896f10d90f07947ed2707091fd39524ed43
Roman S. <roman.novodvorskiy@gmail.com>
2024-02-23 10:13:21 +0200

    v7.7.6
    Remove buymeacoffee link due to Google Policy ban

--- app/src/main/java/ua/napps/scorekeeper/settings/AboutActivity.java
+++ app/src/main/java/ua/napps/scorekeeper/settings/AboutActivity.java
@@ -17,7 +17,6 @@ import androidx.appcompat.widget.Toolbar;
 import timber.log.Timber;
 import ua.napps.scorekeeper.R;
 import ua.napps.scorekeeper.utils.DonateDialog;
-import ua.napps.scorekeeper.utils.Utilities;
 import ua.napps.scorekeeper.utils.ViewUtil;
 
 public class AboutActivity extends AppCompatActivity {
@@ -41,9 +40,6 @@ public class AboutActivity extends AppCompatActivity {
         ((TextView) findViewById(R.id.content)).setMovementMethod(LinkMovementMethod.getInstance());
         findViewById(R.id.image_hero).setOnClickListener(v -> Toast.makeText(this, "Привіт з України \uD83C\uDDFA\uD83C\uDDE6", Toast.LENGTH_SHORT).show());
 
-        findViewById(R.id.btn_rate_it).setOnClickListener(v -> {
-            Utilities.rateApp(this);
-        });
         findViewById(R.id.hero_image).setOnClickListener(v -> {
             DonateDialog dialog = new DonateDialog();
             dialog.show(getSupportFragmentManager(), "donate");
@@ -52,16 +48,16 @@ public class AboutActivity extends AppCompatActivity {
             DonateDialog dialog = new DonateDialog();
             dialog.show(getSupportFragmentManager(), "donate");
         });
-        findViewById(R.id.btn_buy_me_a_coffee).setOnClickListener(v -> {
-            Intent viewIntent =
-                    new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.buymeacoffee.com/score_counter_app"));
-            try {
-                startActivity(viewIntent);
-            } catch (Exception e) {
-                Toast.makeText(this, R.string.message_app_not_found, Toast.LENGTH_SHORT).show();
-                Timber.e(e, "Launch web intent");
-            }
-        });
+//        findViewById(R.id.btn_buy_me_a_coffee).setOnClickListener(v -> {
+//            Intent viewIntent =
+//                    new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.buymeacoffee.com/score_counter_app"));
+//            try {
+//                startActivity(viewIntent);
+//            } catch (Exception e) {
+//                Toast.makeText(this, R.string.message_app_not_found, Toast.LENGTH_SHORT).show();
+//                Timber.e(e, "Launch web intent");
+//            }
+//        });
         findViewById(R.id.btn_help_translate).setOnClickListener(v -> launchEmailClient());
         findViewById(R.id.btn_privacy_policy).setOnClickListener(v -> {
                     Intent viewIntent =
